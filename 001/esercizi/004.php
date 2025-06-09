14fffeb2425082ba4f0725bcb04f51675e1d606e
francesco mansi <f.mansi@socialwebsolutions.it>
2025-06-09 15:42:15 +0200

    Aggiunto 004

---
+++ 001/esercizi/004.php
@@ -0,0 +1,11 @@
+<?php
+// Correggere eventuali errori di dichiarazione e stampare in un **echo** la stringa:
+// *Marco hai sete? Perchè hai bevuto tutto*
+$1text = "Marco";
+$text2 = "hai";
+$text.3 = "sete";
+$text4 = "?";
+@text5 = "Perchè";
+$te-xt6 = '$text2';
+$text 7 = 'bevuto';
+$text8 = "tutto"
